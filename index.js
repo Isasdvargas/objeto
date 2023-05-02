85f9d9e111c8186d0cc1d83e9fb5bb5760ff20cf
Isadora vargas <isadorasilveiradevragas6@gmail.com>
2023-05-02 20:32:17 -0300

    aula de objetos

---
+++ index.js
@@ -0,0 +1,29 @@
+//exercício 1
+//const professores = [
+//    (nome: 'Isadora', modulo: 1),
+//    (nome: 'Arthur' , modulo: 2 )
+//]
+//console.log(professores);
+//console.log(professores[1]);
+//console.log(professores[2], nome);
+
+//exercício 2 
+//const curso = {
+//  nome: "Frontend"
+//}
+//curso.numeresEstudantes = 50;
+//curso['numeroEstudantes'] = 80;
+//console.log(curso.nome)
+
+//exercício 3
+const novofilme = {
+    presidente: "Alberto",
+    treinador: "Renato",
+    nome: "Campeão Brasileiro 2023",
+    ano: 2023,
+    elenco: ["Grando", "Geromel", "Kanemann", "Luís"],
+    vizualizado: "N"
+}
+novofilme.personagens = ['Donatelo', 'Leonardo', 'Rafael', 'Michelangelo'];
+console.log(novofilme.elenco[0], 'como', novofilme.personagens[0]);
+
